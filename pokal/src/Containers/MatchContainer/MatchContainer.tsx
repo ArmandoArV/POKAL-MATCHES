0b28a5b5b7e177ce601637574e9229300120bd9c
ArmandoAV <armando.arredondo.valle@gmail.com>
2023-10-03 09:30:20 -0600

    Implements slider to the MatchContainer

---
+++ pokal/src/Containers/MatchContainer/MatchContainer.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useEffect, useState, useCallback } from "react";
+import MatchComponent from "@/Components/MatchComponent/MatchComponent";
+import { API_URL } from "@/Constants";
+
+interface MatchData {
+  homeTeamNickname: string;
+  awayTeamNickname: string;
+  homeTeamScore?: number | undefined;
+  awayTeamScore?: number | undefined;
+  homePenaltyScore: number;
+  awayPenaltyScore: number;
+}
+
+interface MatchContainerProps {
+  intervalMilliseconds: number; // Prop to specify the interval duration
+}
+
+const DEFAULT_INTERVAL_MS = 5000; // Default interval is 5000 ms (5 seconds)
+
+export default function MatchContainer({ intervalMilliseconds }: MatchContainerProps) {
+  const bearerToken = "95e5e205-8f52-4308-a611-67c72bc7a9b8";
+
+  const [matchData, setMatchData] = useState([] as any); // Initialize state to store match data
+  const [visibleMatches, setVisibleMatches] = useState(3); // Number of matches to display at a time
+  const [startIndex, setStartIndex] = useState(0); // Starting index of matches to display
+
+  const handleNextMatches = () => {
+    const newIndex = (startIndex + visibleMatches) % matchData.length;
+    setStartIndex(newIndex);
+  };
+
+  const handlePrevMatches = () => {
+    const newIndex = startIndex - visibleMatches;
+    if (newIndex >= 0) {
+      setStartIndex(newIndex);
+    } else {
+      setStartIndex(matchData.length - visibleMatches);
+    }
+  };
+
+  useEffect(() => {
+    fetch(`${API_URL}/match/list/matchday/ec1f74c9-0f19-4742-92b8-a9458f90c30a`, {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (!Array.isArray(data.data) || data.data.length === 0) {
+          console.log("Data is empty or undefined.");
+          return;
+        }
+        const matchData = data.data.map((match: any) => ({
+          homeTeamNickname: match.homeTeam?.nickName || "No nickname",
+          awayTeamNickname: match.awayTeam?.nickName || "No nickname",
+          homeTeamScore: match.homeScore || undefined,
+          awayTeamScore: match.awayScore || undefined,
+          homePenaltyScore: match.homePenaltyScore || 1,
+          awayPenaltyScore: match.awayPenaltyScore || 1,
+        }));
+        setMatchData(matchData); // Set the state with the fetched match data
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      handleNextMatches();
+    }, intervalMilliseconds || DEFAULT_INTERVAL_MS);
+
+    return () => clearInterval(intervalId);
+  }, [startIndex, intervalMilliseconds, matchData]);
+
+  const visibleMatchData = matchData.slice(startIndex, startIndex + visibleMatches);
+
+  return (
+    <div>
+      <div>
+        <button onClick={handlePrevMatches}>Previous</button>
+        <button onClick={handleNextMatches}>Next</button>
+      </div>
+      {visibleMatchData.map((match: MatchData, index: number) => (
+        <MatchComponent
+          key={index}
+          homeTeamName={match.homeTeamNickname}
+          awayTeamName={match.awayTeamNickname}
+          timeStart="20:00"
+          dateStart="21/10/2021"
+          homeScore={match.homeTeamScore || 0}
+          awayScore={match.awayTeamScore || 0}
+          extraScoreHome={match.homePenaltyScore}
+          extraScoreAway={match.awayPenaltyScore}
+        />
+      ))}
+    </div>
+  );
+}
--- pokal/src/app/clasificacion/page.tsx
+++ pokal/src/app/clasificacion/page.tsx
@@ -1,61 +1,8 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./page.module.css";
-import MatchComponent from "@/Components/MatchComponent/MatchComponent";
-import { API_URL } from "@/Constants";
-
-interface MatchData {
-  homeTeamNickname: string;
-  awayTeamNickname: string;
-  homeTeamScore?: number | undefined;
-  awayTeamScore?: number | undefined;
-  homePenaltyScore: number;
-  awayPenaltyScore: number;
-}
+import MatchContainer from "@/Containers/MatchContainer/MatchContainer";
 
 export default function Clasificacion() {
-  const bearerToken = "95e5e205-8f52-4308-a611-67c72bc7a9b8";
-
-  const [matchData, setMatchData] = useState([] as any); // Initialize state to store match data
-
-  useEffect(() => {
-    fetch(
-      `${API_URL}/match/list/matchday/ec1f74c9-0f19-4742-92b8-a9458f90c30a`,
-      {
-        headers: {
-          Authorization: `Bearer ${bearerToken}`,
-        },
-      }
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log("Data:", data);
-        if (!Array.isArray(data.data) || data.data.length === 0) {
-          console.log("Data is empty or undefined.");
-          return;
-        }
-        const matchData = data.data.map((match: any) => ({
-          homeTeamNickname: match.homeTeam?.nickName || "No nickname",
-          awayTeamNickname: match.awayTeam?.nickName || "No nickname",
-          homeTeamScore: match.homeScore || undefined,
-          awayTeamScore: match.awayScore || undefined,
-          homePenaltyScore: match.homePenaltyScore || 1,
-          awayPenaltyScore: match.awayPenaltyScore || 1,
-        }));
-
-        console.log("Match Nicknames:", matchData);
-        setMatchData(matchData); // Set the state with the fetched match data
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  }, []);
-
   return (
     <>
       <div className={styles.outerContainer}>
@@ -63,19 +10,7 @@ export default function Clasificacion() {
           <div className={styles.leftContainer}>
             <h1 className={styles.title}>Partidos Actuales</h1>
             <div className={styles.partidosContainer}>
-              {matchData.map((match: MatchData, index: number) => (
-                <MatchComponent
-                  key={index}
-                  homeTeamName={match.homeTeamNickname}
-                  awayTeamName={match.awayTeamNickname}
-                  timeStart="20:00"
-                  dateStart="21/10/2021"
-                  homeScore={match.homeTeamScore || 0}
-                  awayScore={match.awayTeamScore || 0}
-                  extraScoreHome={match.homePenaltyScore}
-                  extraScoreAway={match.awayPenaltyScore}
-                />
-              ))}
+              <MatchContainer intervalMilliseconds={5000} />
             </div>
           </div>
           <div className={styles.rightContainer}>
